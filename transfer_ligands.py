5225d19747c74da73f31d3047d756d539ef02261
Russell Bruce Davidson <RBD@Russells-MacBook-Pro.local>
2016-06-12 12:11:12 -0600

    added a new sys.argv to read in the specific cutoff you want to use (can be any number); tested various cutoffs (1.5,2,2.5) to show how waters are removed as the cutoff is increased

--- transfer_ligands.py
+++ transfer_ligands.py
@@ -2,23 +2,22 @@
 # ----------------------------------------
 # USAGE:
 
-# ./transfer_ligands.py homol_model.pdb template_ligands.pdb output.pdb
+# ./transfer_ligands.py homol_model.pdb template_ligands.pdb output.pdb  cutoff_value
 
 # ----------------------------------------
 # PREAMBLE:
 
 import numpy as np
 import sys
-import os
 import MDAnalysis
-from MDAnalysis.analysis.align import *
 
-pdb1 = sys.argv[1]		# homology model structure
+pdb1 = sys.argv[1]		# homology model structure (so has no ligands)
 pdb2 = sys.argv[2]		# template structure W/ the ligands to be transfered
 output = sys.argv[3]		# name of the pdb file to be created by this script
+cutoff = sys.argv[4]		# number to be used as the cutoff to remove waters within this distance; can be a float or int; good range: 1.5 to 2.5
 
 selection1 = 'protein'		# edit this line if you want to change the atom selection to be used for pdb 1
-selection2 = 'not protein'	# edit this line if you want to change the atom selection to be used for pdb 2
+selection2 = 'not (protein or resname GOL)'	# edit this line if you want to change the atom selection to be used for pdb 2
 
 # ----------------------------------------
 # SUBROUTINES:
@@ -33,7 +32,7 @@ v = MDAnalysis.Universe('%s' %(pdb2))			# Initialize a MDAnalysis.Universe, call
 v_selection = v.select_atoms(selection2)		# creating the AtomGroup object with the desired atom selection
 
 merged = MDAnalysis.Merge(u_selection,v_selection)	# Merge AtomGroups from the two Universes; creates a Universe with all atoms that were merged
-merged_all = merged.select_atoms('not (resname HOH and around 1.5 protein)')			# create a new AtomGroup that includes all atoms
+merged_all = merged.select_atoms('not (resname HOH and around %s protein)' %(cutoff))			# create a new AtomGroup that includes all atoms
 
 merged_all.write(output)				# write a pdb file of the merged universe
 
